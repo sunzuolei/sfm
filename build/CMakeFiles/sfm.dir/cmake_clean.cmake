4a2e6023842b1ae00836ef621de7b352ce492fcb
xiaoqiang <13262528983@qq.com>
2017-05-03 10:10:32 +0800

    此程序最好在ubuntu14.04中运行，若在16.04中可能会出现不能识别Eigen库的问题，vtk最好选择<7的版本。其他的可以参照opencv中官网对于这部分代码的介绍vtk最好选择<7的版本。其他的可以参照opencv中官网对于这部分代码的介绍

---
+++ build/CMakeFiles/sfm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfm.dir/main.cpp.o"
+  "sfm.pdb"
+  "sfm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
